83e5bf252d2561f51816cc260cd702729d891669
Andrew Waller <andrewhwaller@gmail.com>
2019-04-10 18:28:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(someValue) {
-  let someValue;
-  if (someValue <= 400) {
+function scuberGreetingForFeet(distance) {
+
+  if (distance <= 400) {
     return 'This one is on me!'
-  } else if (someValue >= 2000) {
+  } else if (distance >= 2000) {
     return 'I will gladly take your thirty bucks.'
   }
 }
