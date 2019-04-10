3b802e8eae52f1639137a9563596309e398da899
Andrew Waller <andrewhwaller@gmail.com>
2019-04-10 18:28:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(someValue) {
   let someValue;
   if (someValue <= 400) {
     return 'This one is on me!'
-  } else if someValue >= 2000 {
+  } else if (someValue >= 2000) {
     return 'I will gladly take your thirty bucks.'
   }
 }
