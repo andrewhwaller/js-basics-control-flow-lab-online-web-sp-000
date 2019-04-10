dfe13aeac330b0a096806aff2a5504f913f08dcd
Andrew Waller <andrewhwaller@gmail.com>
2019-04-10 18:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(distance) {
 
   if (distance <= 400) {
     return "This one is on me!";
-  } else if (distance >= 2000) {
+  } else if (distance > 1999 && distance < 2501) {
     return "I will gladly take your thirty bucks.";
   } else {
     return "No can do.";
