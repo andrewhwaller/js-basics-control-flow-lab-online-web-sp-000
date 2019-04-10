7eb6c7b9264e2b571ead934310957b8343b77555
Andrew Waller <andrewhwaller@gmail.com>
2019-04-10 18:27:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Write your code in this file!
 
 function scuberGreetingForFeet(someValue) {
-  let scuberGreetingForFeet;
   let someValue;
   if someValue <= 400 {
-    scuberGreetingForFeet = 'This one is on me!'
+    return 'This one is on me!'
   } else if someValue >= 2000 {
-    scuberGreetingForFeet = 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.'
   }
 }
